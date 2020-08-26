9f98173673120aa8e25fb537ae52229b3a7f9153
OmkarShelar9200 <70212432+OmkarShelar9200@users.noreply.github.com>
2020-08-26 18:56:24 +0530

    Update tbot.py

--- tbot.py
+++ tbot.py
@@ -2,10 +2,10 @@ import tweepy
 import os # operating system library
 
 def create_api():
-  consumer_key = 'LcYClGwdDYq9axyCkB4PeT4Rq'
-  consumer_secret = 'rZxPfggFEpTQgjm5uj0Ihf8juoFGHX2lQzFj4n5UGjtlffEfJz'
-  access_token = '1292097403873443842-KsDUsiGquUuLVZNc4FlzF82yT1dmTu'
-  access_token_secret ='zOVqtvSLXCi5M1fVM9YPtRQYPMtVUN5KLUcLGBIOLyD1C'
+  consumer_key = os.getenv('consumer_key')
+  consumer_secret = os.getenv('consumer_secret')
+  access_token = os.getenv('access_token')
+  access_token_secret = os.getenv('access_token_secret')
 
   auth = tweepy.OAuthHandler(consumer_key, consumer_secret)
   auth.set_access_token(access_token, access_token_secret)
@@ -21,9 +21,9 @@ def follower_count(user):
   emoji_numbers =  {0: "0️⃣", 1: "1️⃣", 2: "2️⃣", 3: "3️⃣",
                       4: "4️⃣", 5: "5️⃣", 6: "6️⃣", 7: "7️⃣", 8: "8️⃣", 9: "9️⃣"}
 
-  uf_split = [int(i) for i in str(user.followers_count)]# Used to seperate 
+  uf_split = [int(i) for i in str(user.followers_count)]# Used to seperate
 
-  emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()]) 
+  emoji_followers = ''.join([emoji_numbers[j] for j in uf_split if j in emoji_numbers.keys()])
   return emoji_followers
 
 api = create_api()
@@ -34,4 +34,3 @@ while True:
     print(f'Updating Twitter Name : OMKAR|{follower_count(user)} Followers')
     print('Waiting to refresh')
     time.sleep(60)
- 
